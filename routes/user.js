0a675dccfa8f62fcf80b647c38c26c75ffa45b49
Ahmed Dawood <ahmeddawood666680@gmail.com>
2022-06-21 22:39:35 +0200

    added logout  and cookies working when reloading

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.get('/', userController.login);
 router.get('/login', userController.login);
 router.get('/~', userController.getPage);
+// router.get('/hello', userController.hello);
+router.get('/logout',userController.logout)
 router.post('/auth', userController.auth);
+
 module.exports = router
