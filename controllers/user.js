64fa0713bdd116d357b3955385f8d01f0523de73
Ahmed Dawood <ahmeddawood666680@gmail.com>
2022-06-24 21:10:18 +0200

    friends logic added

--- controllers/user.js
+++ controllers/user.js
@@ -147,7 +147,7 @@ module.exports = {
   friends: async (req, res) => {
       var username=globuser;
       var title='These are your friends, ';
-      const result = await sqlConn.promise().query(`SELECT follow from followers where username = '${username}'`);
+      const result = await sqlConn.promise().query(`select a.follow from followers a,followers b where a.username='${username}' and '${username}'=b.follow and a.follow=b.username and a.username!=a.follow`);
       console.log(globuser+"'s friends are",result[0]);
       res.render('friends',{username,title,result})
       },
@@ -175,7 +175,7 @@ module.exports = {
     var username = globuser;
     let my_posts=['My Posts:'];
     const my_fetched_posts = await sqlConn.promise().query(`SELECT username,post,datetime from posts where username = '${username}' order by datetime DESC`);
-    const result = await sqlConn.promise().query(`SELECT follow from followers where username = '${username}'`);
+    const result = await sqlConn.promise().query(`select a.follow from followers a,followers b where a.username='${username}' and '${username}'=b.follow and a.follow=b.username and a.username!=a.follow`);
     // console.log(my_fetched_posts);
     for (let i = 0; i < my_fetched_posts[0].length; i++) {
       my_posts.push(my_fetched_posts[0][i]['datetime']+', '+my_fetched_posts[0][i]['username']+':   '+my_fetched_posts[0][i]['post']);
